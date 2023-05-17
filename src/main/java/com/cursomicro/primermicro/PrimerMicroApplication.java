7d37c0b267cd576dbab9051c8e9cd0f1398b15fd
David <david.lopez@puntored.co>
2023-05-16 23:07:08 -0500

    first commit

---
+++ src/main/java/com/cursomicro/primermicro/PrimerMicroApplication.java
@@ -0,0 +1,13 @@
+package com.cursomicro.primermicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimerMicroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cursomicro/primermicro/PrimerMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursomicro.primermicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerMicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
